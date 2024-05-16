d3d1eccaaf5c2112e4060441e04e2444455a7a2c
Gurka-202 <lerusya2003@gmail.com>
2024-05-16 15:04:46 +0300

    first commit

---
+++ stack.cpp
@@ -0,0 +1,66 @@
+#include "stack.h"
+#include <iostream>
+
+
+Stack::Stack() {}
+
+
+Stack::Stack(const Stack& other) : stack(other.stack) {}
+
+
+Stack& Stack::operator=(const Stack& other) {
+    if (this != &other) {
+        stack = other.stack;
+    }
+    return *this;
+}
+
+
+void Stack::push(int elem) {
+    stack.push_back(elem);
+}
+
+
+int Stack::pop() {
+    if (isEmpty()) {
+        std::cerr << "Error. Stack is empty" << std::endl;
+        return -1; // або будь-яке інше значення, що позначає помилку
+    }
+    int topElem = stack.back();
+    stack.pop_back();
+    return topElem;
+}
+
+
+bool Stack::isEmpty() const {
+    return stack.empty();
+}
+
+
+void Stack::multiPop(int N) {
+    for (int i = 0; i < N; ++i) {
+        if (!isEmpty()) {
+            pop();
+        }
+        else {
+            std::cerr << "Stack is empty" << std::endl;
+            break;
+        }
+    }
+}
+
+
+void Stack::show() const {
+    if (isEmpty()) {
+        std::cout << "Stack is empty" << std::endl;
+        return;
+    }
+
+    for (size_t i = 0; i < stack.size(); ++i) {
+        std::cout << stack[i];
+        if (i < stack.size() - 1) {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+}
